e11a8e9b738a85f142ea6c905bb7c9929fd85d6d
Narek97 <narek.babayan.97.27@gmail.com>
2024-10-24 12:44:09 +0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: `${process.env.NEXT_PUBLIC_MAIN_DOMAIN}|192.168.10.141`,
+          },
+        ],
+        destination: '/main/:path*',
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: `${process.env.NEXT_PUBLIC_ADMIN_DOMAIN}|192.168.10.141`,
+          },
+        ],
+        destination: '/admin/:path*',
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
